483e9675fea569b9a25d88ca87bde82c884a7705
nahom-23 <noahbirhanu23@gmail.com>
2025-01-24 09:53:46 +0300

    update the route

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import LoginPage from "./Component/LoginPage"; // You can create a HomePage comp
 import LoginForm from "./Component/LoginForm";
 
 import { ChatView } from "./Component/Chatbot/ChatView";
+import Index from "./layout/admin/Index";
 import ErrorBoundary from "./Component/Chatbot/ErrorBoundary";
 
 import Profile from "./Component/profile";
-
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -22,6 +22,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
+      <Route path="/dashbord" element={<Index />}></Route>
       <Route path="/about" element={<Header />}></Route>
       <Route path="/how-it-works" element={<Header />}></Route>
       <Route path="/contact" element={<Header />}></Route>
@@ -36,11 +37,8 @@ const App = () => {
 
       <Route path="/Forgot Password" element={<LoginPage />}></Route>
 
-     
-
-
-      <Route path="/Sign Up" element={<LoginForm />} ></Route>
-    <Route path="/profile" element={<Profile/>}/>
+      <Route path="/Sign Up" element={<LoginForm />}></Route>
+      <Route path="/profile" element={<Profile />} />
 
       <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
     </Routes>
