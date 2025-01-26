2e7b5c3e53fbc20fc9f49e71d59396e454ab32fc
Michael Garedew <118183428+MIKEY1221@users.noreply.github.com>
2025-01-26 17:20:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
 
       />
 
-      ></Route>
+      </Route>
       <Route path="/connect" element={<Connect />} />
 
       <Route path="/Forgot Password" element={<LoginPage />}></Route>
