9241e4c2c4c55d4734a757715cc6b5e46b9b6427
Michael Garedew <118183428+MIKEY1221@users.noreply.github.com>
2025-01-26 17:36:49 +0300

    Update LoginForm.js

--- src/Component/LoginForm.js
+++ src/Component/LoginForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-import { Link } from "react-router-dom"; // Ensure Link is imported
+//import { Link } from "react-router-dom"; // Ensure Link is imported
 
-// import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const LoginForm = ({ onLogin }) => {
